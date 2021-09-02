643328e3e1d79ac3ef1d5b30ef9b848c286c8dd4
pbtripletree <paul@tripletreellc.com>
2021-09-02 16:17:57 -0600

    register applications service

--- src/SendBird.js
+++ src/SendBird.js
@@ -1,35 +1,38 @@
-'use strict';
+"use strict";
 
-const rp = require('request-promise');
+const rp = require("request-promise");
 
-const DEFAULT_BASE_URL = 'https://api.sendbird.com/v3/';
+const DEFAULT_BASE_URL = "https://api.sendbird.com/v3/";
 
 // users
-const usersService = require('./services/users');
+const usersService = require("./services/users");
 // open channel
-const openChannelsService = require('./services/openChannels');
+const openChannelsService = require("./services/openChannels");
 // group channel
-const groupChannelsService = require('./services/groupChannels');
+const groupChannelsService = require("./services/groupChannels");
+// applications
+const applicationsService = require("./services/applications");
 
 function SendBird(apiToken, baseUrl = DEFAULT_BASE_URL) {
-    if (!apiToken) {
-        throw new Error('Invalid api token');
-    }
-    const request = rp.defaults({
-        baseUrl,
-        headers: {
-            'Api-Token': apiToken
-        },
-        json: true
-    });
+  if (!apiToken) {
+    throw new Error("Invalid api token");
+  }
+  const request = rp.defaults({
+    baseUrl,
+    headers: {
+      "Api-Token": apiToken,
+    },
+    json: true,
+  });
 
-    const api = {};
+  const api = {};
 
-    api.users = usersService(request);
-    api.openChannels = openChannelsService(request);
-    api.groupChannels = groupChannelsService(request);
+  api.users = usersService(request);
+  api.openChannels = openChannelsService(request);
+  api.groupChannels = groupChannelsService(request);
+  api.applications = applicationsService(request);
 
-    return api;
+  return api;
 }
 
 module.exports = SendBird;
