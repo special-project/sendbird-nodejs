ca103fc0824f91f415031d1ea20abdfcec2fd778
pbtripletree <paul@tripletreellc.com>
2021-09-02 17:17:23 -0600

    ban user

--- src/services/applications/actions.js
+++ src/services/applications/actions.js
@@ -7,6 +7,10 @@ const actions = {
     method: GET,
     endpoint: "/settings_by_channel_custom_type/{custom_type}/operators",
   },
+  listOperators: {
+    method: POST,
+    endpoint: "/settings_by_channel_custom_type/{custom_type}/ban",
+  },
 };
 
 module.exports = actions;
