fd276e8a0230f2ba83baea3db0255bef0dc5474c
pbtripletree <paul@tripletreellc.com>
2021-09-07 16:27:31 -0600

    uban support

--- src/services/applications/actions.js
+++ src/services/applications/actions.js
@@ -15,6 +15,10 @@ const actions = {
     method: GET,
     endpoint: "/settings_by_channel_custom_type/{custom_type}/ban",
   },
+  unbanUsers: {
+    method: DELETE,
+    endpoint: "/settings_by_channel_custom_type/{custom_type}/ban",
+  },
 };
 
 module.exports = actions;
