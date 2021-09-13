9dd699e681ef172546e3241101f34a86f353fe6a
pbtripletree <paul@tripletreellc.com>
2021-09-13 15:31:26 -0600

    mute support

--- src/services/applications/actions.js
+++ src/services/applications/actions.js
@@ -20,6 +20,19 @@ const actions = {
     endpoint:
       "/settings_by_channel_custom_type/{custom_type}/ban?user_ids={user_ids}",
   },
+  muteUsers: {
+    method: POST,
+    endpoint: "/settings_by_channel_custom_type/{custom_type}/mute",
+  },
+  listMutedUsers: {
+    method: GET,
+    endpoint: "/settings_by_channel_custom_type/{custom_type}/mute",
+  },
+  unmuteUsers: {
+    method: DELETE,
+    endpoint:
+      "/settings_by_channel_custom_type/{custom_type}/mute?user_ids={user_ids}",
+  },
 };
 
 module.exports = actions;
